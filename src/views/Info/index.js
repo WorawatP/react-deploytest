d3dce596c9fbcd6026bd0622d244c77befe40163
heartbeetbug@gmail.com <heartbeetbug@gmail.com>
2019-11-20 23:45:04 +0700

    fix index.js in Info file

--- src/views/Info/index.js
+++ src/views/Info/index.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import { NavBar } from '../../components'
 import trim from 'trim';
-import { Button } from 'react-bootstrap'
+import { Modal, Button, Form, Container, Row, Col } from 'react-bootstrap'
 import { db } from '../../firebase'
 import {
     Spinner,
 } from 'react-bootstrap';
-
-export default class Info extends Component {
+export default class Home extends Component {
     constructor(props) {
         super(props);
         this.idCount = 0;
@@ -25,6 +24,17 @@ export default class Info extends Component {
             idToEdit: '',
         }
     }
+    BeforSubmit = async () => {
+        this.setState({ 
+            isEdit: false,
+            name: '',
+            address: '',
+            time: '',
+            order: '',
+        })
+        this.handleModal()
+    }
+
     onSubmit = async () => {
         const { data } = this.state;
         await db.collection('test').doc(data ? this.state.lastId + 1 + '' : '0').set({
@@ -35,7 +45,9 @@ export default class Info extends Component {
             order: this.state.order,
         })
         this.getData();
+        this.handleModal();
     }
+
     getData = async () => {
         this.setState({ isLoading: true })
         await db.collection('test').get()
@@ -60,7 +72,7 @@ export default class Info extends Component {
             })
     }
     onDelete = async (id) => {
-        await db.collection('test').doc(id + '').delete()
+        await db.collection('test').doc(id + '').delete();
         await this.getData();
     }
     componentDidMount = async () => {
@@ -71,45 +83,41 @@ export default class Info extends Component {
             return a.id - b.id;
         })
         return tempData.map((e, i) => {
-            return <div key={i} style={{ width: '100%', display: 'flex', flexDirection: 'row', marginTop: 8 }}>
-                <div style={{ width: '14%' }}>{e.id}</div>
-                <div style={{ width: '14%' }}>{e.name}</div>
-                <div style={{ width: '14%' }}>{e.address}</div>
-                <div style={{ width: '14%' }}>{e.time}</div>
-                <div style={{ width: '14%' }}>{e.order}</div>
-                <div id = "move1" style={{ width: '5%' }}>
-                    <Button variant="outline-danger" onClick={() => this.onDelete(e.id)} disabled={this.state.isEdit}>
-                        <i class ="fa fa-trash"></i>
-                    </Button>
+            return <div key={i} style={{ width: '100%', display: 'flex', flexDirection: 'row', marginTop: 8, minWidth: 500 }}>
+                {/* <div style={{ width: '14%' }}>{e.id}</div> */}
+                <div style={{ width: '18%' }}>{e.name}</div>
+                <div style={{ width: '18%' }}>{e.address}</div>
+                <div style={{ width: '18%' }}>{e.time}</div>
+                <div style={{ width: '18%' }}>{e.order}</div>
+                <div id="move1" style={{ width: '5%' }}>
+                    {/* <Button variant="outline-danger" onClick={() => this.onDelete(e.id)} disabled={this.state.isEdit}> */}
+                    {!!!this.state.isLoading &&
+                        <Button variant="outline-danger" onClick={() => this.onDelete(e.id)} >
+                            <i class="fa fa-trash"></i>
+                        </Button>
+                    }
                 </div>
                 <div style={{ width: '0%' }}>
-                    <Button variant="outline-warning" onClick={() => this.handleEdit(e)}>
-                        <i class="fa fa-edit"></i>
-                    </Button>
+                    {!!!this.state.isLoading &&
+                        <Button variant="outline-warning" onClick={() => this.handleEdit(e)}>
+                            <i class="fa fa-edit"></i>
+                        </Button>
+                    }
                 </div>
             </div>
         })
     }
-
     handleEdit = (e) => {
+        console.log('get e from table', e)
         this.setState({
             name: e.name,
             address: e.address,
             time: e.time,
             order: e.order,
-            isEdit: !this.state.isEdit,
+            isEdit: true,
             idToEdit: e.id,
         })
-
-        if (this.state.isEdit) {
-            this.setState({
-                name: '',
-                address: '',
-                time: '',
-                order: '',
-                // idToEdit: ''
-            })
-        }
+        this.handleModal();
     }
     onEdit = async () => {
         await db.collection('test').doc(+this.state.idToEdit + '').set({
@@ -128,9 +136,19 @@ export default class Info extends Component {
             idToEdit: '',
         })
         this.getData();
+        this.handleModal();
     }
-    render() {
 
+    handleModal = (id, e) => {
+        this.setState({
+            modalVisible: !this.state.modalVisible
+        })
+    }
+
+
+
+    render() {
+        console.log(this.state.isEdit)
         return (
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingLeft: 350, height: '100%', flexDirection: 'column', width: '100%' }}>
                 {/* <div>lasted id is {this.state.lastId + 1}</div> */}
@@ -138,7 +156,7 @@ export default class Info extends Component {
                     <div style={{ width: '10%' }}>
                         {/* <input  /> */}
                     </div>
-                    <div style={{ width: '18%' }}>
+                    {/* <div style={{ width: '18%' }}>
                         <input placeholder="Name" value={this.state.name} onChange={e => this.setState({ name: e.target.value })} />
                     </div>
                     <div style={{ width: '18%' }}>
@@ -149,41 +167,66 @@ export default class Info extends Component {
                     </div>
                     <div style={{ width: '15%' }}>
                         <input placeholder="Order" value={this.state.order} onChange={e => this.setState({ order: e.target.value })} />
-                    </div>
+                    </div> */}
 
-                    <div style={{ width: '12%' }}>
-
+                    <div style={{ width: '14%' }}></div>
+                </div>
+                <div style={{ width: '100%', display: 'flex', flexDirection: 'row', marginTop: 31 , minWidth:500 }}>
+                    {/* <div id = "font" style={{ width: '14%' }}>id</div> */}
+                    <div id="font" style={{ width: '18%' }}>name</div>
+                    <div id="font" style={{ width: '18%' }}>address</div>
+                    <div id="font" style={{ width: '18%' }}>time</div>
+                    <div id="font" style={{ width: '18%' }}>order</div>
+                    <div style={{ width: '11%' }}>
                         {
                             this.state.isLoading ?
-                                <Button id ="butin" variant="primary">
+                                <Button id="butin" variant="primary">
                                     <Spinner animation="border" role="status">
                                         <span className="sr-only">Loading...</span>
                                     </Spinner>
                                 </Button>
                                 :
-                                <Button id ="butin" variant="primary" onClick={() => {
-                                    this.state.isEdit ? this.onEdit() : this.onSubmit()
-                                }}>
-                                    {this.state.isEdit ? 'edit' : 'add'}
-                                </Button>
+                                <Button id="butin" onClick={() => this.BeforSubmit()}>Add</Button>
                         }
-
                     </div>
-
                     <div style={{ width: '14%' }}></div>
                 </div>
-                <div style={{ width: '100%', display: 'flex', flexDirection: 'row', marginTop: 31 }}>
-                    <div id = "font" style={{ width: '14%' }}>id</div>
-                    <div id = "font" style={{ width: '14%' }}>name</div>
-                    <div id = "font" style={{ width: '14%' }}>address</div>
-                    <div id = "font" style={{ width: '14%' }}>time</div>
-                    <div id = "font" style={{ width: '14%' }}>order</div>
-                    <div style={{ width: '14%' }}></div>
-                    <div style={{ width: '14%' }}></div>
-                </div>
-                <div style={{ height: 1, backgroundColor: 'grey', width: '100%', marginRight: 100, marginTop: 16, marginBottom: 16 }}></div>
-                <div style={{ width: '100%' }}>
+                <div style={{ height: 1, backgroundColor: 'grey', width: '100%', marginRight: 100, marginTop: 16, marginBottom: 16, minWidth: 500 }}></div>
+                <div style={{ width: '100%', minWidth: 500 }}>
                     {this.renderTable()}
+                    <Modal show={this.state.modalVisible} onHide={() => this.handleModal()}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>{this.state.isEdit ? 'Edit Information' : 'Information'}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Name</Form.Label>
+                                    <Form.Control onChange={e => this.setState({ name: e.target.value })} value={this.state.name} type="text" placeholder="enter name" />
+
+                                    <Form.Label>Address</Form.Label>
+                                    <Form.Control onChange={e => this.setState({ address: e.target.value })} value={this.state.address} type="text" placeholder="enter address" />
+
+                                    <Form.Label>Time</Form.Label>
+                                    <Form.Control onChange={e => this.setState({ time: e.target.value })} value={this.state.time} type="time" placeholder="enter time" />
+
+                                    <Form.Label>Order</Form.Label>
+                                    <Form.Control onChange={e => this.setState({ order: e.target.value })} value={this.state.order} type="text" placeholder="enter order" />
+
+                                </Form.Group>
+                            </Form>
+
+                        </Modal.Body>
+
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={() => this.handleModal()}>
+                                Close
+                            </Button>
+                            <Button variant="primary" onClick={() => { this.state.isEdit ? this.onEdit() : this.onSubmit() }}>
+                                {this.state.isEdit ? 'Save Changes' : 'Submit'}
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
                 </div>
             </div>
         )
